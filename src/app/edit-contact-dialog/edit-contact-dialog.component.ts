75f69236ed863b92fed82c6087f3fabc4b1e49f9
hafsanazia <hafsanazia94@gmail.com>
2019-08-13 12:22:07 +0530

    validated phone number

--- src/app/edit-contact-dialog/edit-contact-dialog.component.ts
+++ src/app/edit-contact-dialog/edit-contact-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormBuilder, FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { Contact } from '../models/contact';
 
 @Component({
@@ -9,14 +10,35 @@ import { Contact } from '../models/contact';
 })
 export class EditContactDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<EditContactDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: Contact) { }
+  userAddressValidations: FormGroup;
+  
+
+  constructor(public dialogRef: MatDialogRef<EditContactDialogComponent>,  
+              @Inject(MAT_DIALOG_DATA) public data: Contact, private formBuilder: FormBuilder ) { 
+              //   this.myGroup = new FormGroup({
+              //     firstName: new FormControl()
+              //  });
+              }
 
   ngOnInit() {
+    this.userAddressValidations = this.formBuilder.group({
+      Name: '',
+      mobileNumber: ['', [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]]
+    });
+
+    
   }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
+  _keyPress(event: any) {
+    const pattern = /[0-9]/;
+    let inputChar = String.fromCharCode(event.charCode);
+    if (!pattern.test(inputChar)) {
+        event.preventDefault();
 
+    }
+  }
+  
 }
